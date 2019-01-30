d0bce8d94cb60b138c6af333353a40dd8e548ef6
CHEN <chengf_28@163.com>
2019-01-30 20:21:21 +0800

    update

--- admin/js/app.js
+++ admin/js/app.js
@@ -1,8 +1,7 @@
 // 生成涟漪效果函数
-function ripple(obj,x,y,width)
+function ripple(obj,x,y,width,scale)
 {
     var ripple = document.createElement('div');
-    var width  = obj.width() > obj.height() ? obj.height() : obj.width();
     $(ripple).css({
         "position"                   : "absolute",
         "border-radius"              : "100%",
@@ -11,8 +10,8 @@ function ripple(obj,x,y,width)
         "transform"                  : "scale(0)",
         "height"                     : width,
         "width"                      : width,
-        "-webkit-transition-duration": "1s",
-        "transition-duration"        : "1s",
+        "-webkit-transition-duration": ".7s",
+        "transition-duration"        : ".7s",
         "top"                        : y,
         "left"                       : x,
         "z-index"                    : 0,
@@ -26,15 +25,15 @@ function ripple(obj,x,y,width)
     $(ripple).animate({},function()
     {
         $(ripple).css({
-            "-webkit-transform": "scale(5)",
-            "transform"        : "scale(5)",
+            "-webkit-transform": "scale("+scale+")",
+            "transform"        : "scale("+scale+")",
              "background-color" : "transparent",
         });
     });
     // 1.2秒后清除多余的dom
     setTimeout(function (){
         $(ripple).remove();
-     },1200);
+     },1000);
 }
 
 // 点击触发涟漪效果
@@ -42,12 +41,21 @@ $('.click-ripple').click(function(e){
     var width = $(this).width() > $(this).height() ? $(this).height() : $(this).width();
     var x = e.pageX - $(this).offset().left - (width/2);
     var y = e.pageY - $(this).offset().top - (width/2);
-    ripple($(this),x,y,width);
+    var scale = 0;
+    if ( $(this).width() > $(this).height() ) 
+    {
+        width = $(this).height();
+        scale = parseInt($(this).width() / width)+1;
+    }else{
+        width = $(this).width();
+        scale = parseInt($(this).height() / width)+1;
+    }
+    ripple($(this),x,y,width,scale);
 });
 
 //  处理侧边
 $('.aside-menu').on('click',function(){
-    var dom       = $('aside');
+    var dom       = $('aside').parents();
     var classname = "sidebar-mini";
     if(dom.hasClass(classname))
     {
@@ -57,17 +65,19 @@ $('.aside-menu').on('click',function(){
     }
 });
 
-// 子菜单栏
-$('.sidebar-title').on('click',function(){
-    var $this = $(this);
-    var dom   = $this.siblings('ul[class=sidebar-child]');
-    var  icon = $this.children('span[class*=oi-chevron-left]');
-    if( icon.length > 0 )
-    {
-        icon.removeClass('oi-chevron-left').addClass('oi-chevron-bottom');
-    }else{
-        $this.children('span[class*=oi-chevron-bottom]').addClass('oi-chevron-left').removeClass('oi-chevron-bottom');
-    }
-    dom.toggle();
 
-})
+$('.sidebar-li').children('div').on('click', function () 
+{
+    var $this = $(this);
+    var otherdom = $this.parents('.sidebar-li').siblings().children('ul[class*=sidebar-child]');
+    otherdom.slideUp(0);
+    var dom = $this.siblings('ul[class*=sidebar-child]');
+    if ($this.parent().hasClass("active")) 
+    {
+        dom.slideUp(0);
+        $this.parent().removeClass("active");
+    } else {
+        // dom.slideDown(300);
+        $this.parent().addClass("active").siblings().removeClass("active");
+    }
+});
